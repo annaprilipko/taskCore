3f3473f50ad1ad819113c9b3e28824d3f3b6e63e
Anna Priliko <anna.prilipkoo@gmail.com>
2019-05-17 22:47:06 +0300

    new

---
+++ task14/task1408/UkrainianHen.java
@@ -0,0 +1,15 @@
+package com.javarush.task.task14.task1408;
+
+public class UkrainianHen extends Hen implements Country{
+
+    @Override
+    int getCountOfEggsPerMonth() {
+        return 0;
+    }
+
+    @Override
+    String getDescription() {
+        return super.getDescription() + " Моя страна - " + UKRAINE +". Я несу " + getCountOfEggsPerMonth() + " яиц в месяц.";
+    }
+
+}
